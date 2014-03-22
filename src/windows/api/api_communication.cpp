1e7c0400eda31dd36a5ae7256e868cdb9868b38c
wuyingfengsui <wuyingfengsui55@sina.com>
2014-03-23 01:36:19 +0800

    通信API

---
+++ src/windows/api/api_communication.cpp
@@ -0,0 +1,310 @@
+#include <boost/assert.hpp>
+#include <gloox/message.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include "api_communication.h"
+
+using std::string;
+using boost::thread;
+using boost::shared_ptr;
+using boost::property_tree::ptree;
+using namespace psychokinesis;
+
+bool api_communication::open() {
+	if (is_open)
+		return true;
+		
+	client = new gloox::Client(server);
+	
+	// 注册连接状态监听器，实现onConnect，onDisconnect和onTLSConnect接口
+	client->registerConnectionListener(this);
+	// 注册会话session监听器，当有新会话建立时会调用handleMessageSession接口
+	client->registerMessageSessionHandler(this, 0);
+	// 注册log监听器，实现handleLog接口
+	client->logInstance().registerLogHandler(gloox::LogLevelDebug, gloox::LogAreaAll, this);
+	
+	configure();
+	
+	is_open = true;
+	
+	client_thread = new thread(run_client_thread, this);
+	
+	debug_print("api_communication open");
+	
+	return true;
+}
+
+
+shared_ptr<ptree> api_communication::execute(const ptree& args) {
+	shared_ptr<ptree> resp(new ptree());
+	
+	try {
+		string opr = args.get<string>("opr");
+		
+		if (opr == "communicate") {
+			ptree content = args.get_child("content");
+			
+			string account = content.get<string>("account");
+			string resource_name = content.get<string>("resource_name");
+			string message = content.get<string>("message");
+			
+			std::string pa(account);
+			pa = pa + "@" + server + "/" + resource_name;
+			gloox::JID jid;
+			if (jid.setJID(pa) == false) {
+				resp->put("ret_code", 2);
+				return resp;
+			}
+
+			gloox::MessageSession* session = new gloox::MessageSession(client, jid);
+			session->send(message);
+			client->disposeMessageSession(session);
+			
+			resp->put("ret_code", 0);
+			return resp;
+		} else if (opr == "configure") {
+			ptree parameters = args.get_child("parameters");
+			
+			server = parameters.get<string>("server");
+			port = parameters.get<int>("port");
+			account = parameters.get<string>("account");
+			password = parameters.get<string>("password");
+			resource = parameters.get<string>("resource");
+			reconnect_timeout = parameters.get<unsigned int>("reconnect_timeout");
+			
+			if (is_open)
+				configure();
+			
+			resp->put("ret_code", 0);
+			return resp;
+		} else {
+			BOOST_ASSERT(0 && "bad json!");
+		}
+		
+	} catch (boost::property_tree::ptree_bad_path) {
+		BOOST_ASSERT(0 && "bad json!");
+	}
+	
+	resp->put("ret_code", 1);
+	return resp;
+}
+
+
+void api_communication::close() {
+	BOOST_ASSERT(is_open == true && "gloox have closed!");
+	
+	is_open = false;
+	client->disconnect();
+	
+	client_thread->join();
+	delete client_thread;
+	client_thread = NULL;
+	
+	delete client;
+	client = NULL;
+	
+	debug_print("api_communication close");
+}
+
+
+void api_communication::handleMessage(const gloox::Message& msg, gloox::MessageSession* session) {
+	ptree content;
+	
+	content.put("account", msg.from().username());
+	content.put("resource_name", msg.from().resource());
+	content.put("message", msg.body());
+	
+	shared_ptr<string> resp = communicate(content);
+	
+	if (resp->length() > 0)
+		session->send(*resp);
+	
+	client->disposeMessageSession(session);
+}
+
+
+void api_communication::onConnect() {
+	ptree content;
+	
+	content.put("info", "connected");
+	
+	info(content);
+
+	debug_print("api_communication connect");
+}
+
+
+void api_communication::onDisconnect(gloox::ConnectionError e) {
+	ptree content;
+	
+	content.put("warning", "disconnect");
+	content.put("error_code", e);
+	
+	warning(content);
+	
+	debug_print("api_communication disconnect");
+}
+
+
+bool api_communication::onTLSConnect(const gloox::CertInfo& info) {
+	return true;                 // 返回true表示接受此TLS连接，如返回false则关闭此连接
+}
+
+
+void api_communication::handleMessageSession(gloox::MessageSession* session) {
+	// 注册此会话的消息监听器，当有新消息到来时会调用handleMessage接口
+	session->registerMessageHandler(this);
+}
+
+
+void api_communication::handleLog(gloox::LogLevel level, gloox::LogArea area, const std::string& message) {
+	debug_print(message);
+}
+
+
+void api_communication::configure() {
+	BOOST_ASSERT(client && "gloox haven't open!");
+	
+	client->disconnect();
+	
+	client->setServer(server);
+	if (port > -1)
+		client->setPort(port);
+	client->setUsername(account);
+	client->setPassword(password);
+	client->setResource(resource);
+	
+	if (!reconnect_semaphore.try_wait())
+		reconnect_semaphore.post();
+}
+
+
+void api_communication::run_client_thread(void* handle) {
+	api_communication* h = (api_communication*)handle;
+	
+	while (h->is_open) {
+		// 调用 j->connect(true)时，即实现与服务器的连接，连接成功会返回真。
+		// connect函数参数为false表示不阻塞方式连接，经测试也会造成短暂阻塞
+		// 而如果为true ，则为阻塞方式连接，即会等待接收数据
+		h->client->connect(true);
+		
+		if (h->is_open)
+			h->reconnect_semaphore.timed_wait(boost::get_system_time() + boost::posix_time::seconds(h->reconnect_timeout));
+	}
+}
+
+
+
+#define TEST
+#ifdef TEST
+
+#include <iostream>
+#include <boost/property_tree/json_parser.hpp>
+
+using namespace std;
+
+class api_communication_listener : public api_listener {
+public:
+	virtual void debug(const ptree& content) {
+		try {
+			cout << "debug: " << content.get<string>("debug") << endl;
+		} catch (boost::property_tree::ptree_bad_path) {
+			cout << "bad json!" << endl;
+			BOOST_ASSERT(0 && "");
+		}
+	}
+	
+	virtual void info(const ptree& content) {
+		stringstream content_str;
+		write_json(content_str, content);
+		
+		cout << "info: " << content_str.str() << endl;
+	}
+	
+	virtual void warning(const ptree& content) {
+		stringstream content_str;
+		write_json(content_str, content);
+		
+		cout << "warning: " << content_str.str() << endl;
+	}
+	
+	virtual void alert(const ptree& content) {
+		stringstream content_str;
+		write_json(content_str, content);
+		
+		cout << "alert: " << content_str.str() << endl;
+	}
+	
+	virtual void communicate(ptree& content) {
+		ptree resp;
+		stringstream content_str;
+		write_json(content_str, content);
+		
+		cout << "communicate: " << content_str.str() << endl;
+		
+		resp.put("response", "ok!");
+		
+		content = resp;
+	}
+};
+
+int main() {
+	api_communication api;
+	api_communication_listener listener;
+	char json_path[260];
+	ptree json;
+	
+	cout << "config json file's path: ";
+	cin.getline(json_path, 260);
+	cout << json_path << endl;
+	if (cin.eof())
+		return 0;
+		
+	try {
+		boost::property_tree::read_json(json_path, json);
+	} catch (...) {
+		cout << "parse config json file failed!" << endl;
+		return 0;
+	}
+		
+	shared_ptr<ptree> result = api.execute(json);
+	
+	if (result->get<int>("ret_code")) {
+		cout << "config failed! ret_code = " << result->get<int>("ret_code") << endl;
+		return 0;
+	}
+	
+	if (api.open() == false) {
+		cout << "open gloox failed!" << endl;
+		return 0;
+	}
+	api.attach_listener(&listener);
+	
+	while (true) {
+		cout << "json file's path: ";
+		cin.getline(json_path, 260);
+		cout << json_path << endl;
+		if (cin.eof())
+			break;
+		
+		try {
+			boost::property_tree::read_json(json_path, json);
+		} catch (...) {
+			cout << "parse json file failed!" << endl;
+			continue;
+		}
+		
+		result = api.execute(json);
+		
+		std::stringstream result_str;
+		write_json(result_str, *result);
+		cout << "the result: " << endl
+			 << result_str.str() << endl;
+	}
+	
+	api.close();
+	api.detach_listener(&listener);
+	
+	return 0;
+}
+
+#endif
---
+++ src/windows/api/api_communication.h
@@ -0,0 +1,53 @@
+#ifndef _API_COMMUNICATION_H_
+#define _API_COMMUNICATION_H_
+
+#include <gloox/client.h>
+#include <gloox/loghandler.h>
+#include <gloox/messagehandler.h>
+#include <gloox/connectionlistener.h>
+#include <gloox/messagesessionhandler.h>
+#include <boost/thread/thread.hpp>
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
+#include "api.h"
+
+namespace psychokinesis {
+
+class api_communication : public api, public gloox::MessageSessionHandler, public gloox::ConnectionListener, public gloox::MessageHandler, public gloox::LogHandler {
+public:
+	api_communication()
+		: client(NULL), client_thread(NULL), 
+		  is_open(false), port(-1), reconnect_timeout(10),
+		  reconnect_semaphore(0)
+	{}
+	virtual bool open();
+	
+	virtual boost::shared_ptr<boost::property_tree::ptree> execute(const boost::property_tree::ptree& args);
+	
+	virtual void close();
+	
+private:
+	virtual void handleMessage(const gloox::Message& msg, gloox::MessageSession* session = 0);
+	virtual void onConnect();
+	virtual void onDisconnect(gloox::ConnectionError e);
+	virtual bool onTLSConnect(const gloox::CertInfo& info);
+	virtual void handleMessageSession(gloox::MessageSession* session);
+	virtual void handleLog(gloox::LogLevel level, gloox::LogArea area, const std::string& message);
+	
+	void configure();
+	static void run_client_thread(void* handle);
+	
+	gloox::Client* client;                                             // 客户端实例对象指针
+	boost::thread* client_thread;                                      // 客户端线程
+	boost::interprocess::interprocess_semaphore reconnect_semaphore;   // 立即重连信号量
+	bool is_open;                                                      // 开启标识
+	
+	std::string server;                        // 服务器域名
+	int port;                                  // 服务器端口号
+	std::string account;                       // 账号名
+	std::string password;                      // 密码
+	std::string resource;                      // 资源名
+	unsigned int reconnect_timeout;            // 重连等待时间
+};
+
+} // namespace psychokinesis
+#endif
