6ee686314a96cc3769c53ae906726d38d22a5b4b
wuyingfengsui <wuyingfengsui55@sina.com>
2014-03-29 00:35:44 +0800

    自适应当前目录

--- src/windows/make.sh
+++ src/windows/make.sh
@@ -1,31 +1,39 @@
-#!/bin/sh
+#! /bin/sh
 
-PROJECT_PATH=$HOME/test/windows
-
-cd ${PROJECT_PATH}
-if [ $? -ne 0 ]; then
-	echo "${PROJECT_PATH} isn't exist!"
-	exit 1
+# 获取当前项目所在目录
+RUN_PATH=`pwd`
+PROJECT_PATH=`echo $0 | grep "^/"`
+if test "${PROJECT_PATH}"; then
+        PROJECT_PATH==`dirname "$0"`
+else
+        PROJECT_PATH=`dirname "${RUN_PATH}/$0"`
 fi
 
-if [ -d "${PROJECT_PATH}/build" ]; then
-	echo "delete the directory: ${PROJECT_PATH}/build"
+echo "Generating Makefile..."
+
+# 生成Makefile文件
+if [ ! -f "${PROJECT_PATH}/build/Makefile" ]; then
 	rm -rf ${PROJECT_PATH}/build
-fi
-
-mkdir ${PROJECT_PATH}/build
-cd ${PROJECT_PATH}/build
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-mingw32.cmake ..
-if [ $? -ne 0 ]; then
+	mkdir ${PROJECT_PATH}/build
+	cd ${PROJECT_PATH}/build
+	cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-mingw32.cmake ..
+	if [ $? -ne 0 ]; then
         echo "cmake failed!"
         exit 2
+	fi
+	cd -
 fi
 
+echo "Compiling..."
+
+# 编译
+cd ${PROJECT_PATH}/build
 make
 if [ $? -ne 0 ]; then
         echo "make failed!"
         exit 3
 fi
+cd -
 
-strip psychokinesis.exe
-sz psychokinesis.exe
+strip ${PROJECT_PATH}/build/psychokinesis.exe
+sz ${PROJECT_PATH}/build/psychokinesis.exe
