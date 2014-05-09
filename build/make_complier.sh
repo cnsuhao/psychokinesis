643f758fbfa3f47ad61818c66e6f69bfc63df169
wuyingfengsui <wuyingfengsui55@sina.com>
2014-05-09 23:43:15 +0800

    添加解压bz2需要的命令

--- build/make_complier.sh
+++ build/make_complier.sh
@@ -2,7 +2,7 @@
 # 基于debian-7.3.0-amd64系统，其他debian系操作系统也应该适用
 #!/bin/bash
 
-NECESSARY_COMMANDS=(gcc make git p7zip ntpdate gcc-mingw32)
+NECESSARY_COMMANDS=(gcc make git p7zip bzip2 ntpdate gcc-mingw32)
 
 for command in ${NECESSARY_COMMANDS[@]};
 do
