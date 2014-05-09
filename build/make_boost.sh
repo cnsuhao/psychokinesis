5e08ced59e54755129a2a875d8f32efbd70357fa
wuyingfengsui <wuyingfengsui55@sina.com>
2014-05-10 01:28:51 +0800

    采用强制覆盖的方法将修改过的boost头文件替换

--- build/make_boost.sh
+++ build/make_boost.sh
@@ -20,4 +20,6 @@ cd ${BOOST_PATH}
 echo "using gcc : mingw32 : i686-w64-mingw32-g++ : <rc>i686-w64-mingw32-windres <archiver>i686-w64-mingw32-ar <ranlib>i686-w64-mingw32-ranlib ;" >> ${BOOST_PATH}/tools/build/v2/user-config.jam
 ./b2 install toolset=gcc-mingw32 target-os=windows threading=multi threadapi=win32 link=static variant=release --prefix=${INSTALL_PATH_PREFIX} --without-mpi
 
+cp -f ../lib/boost/property_tree/detail/json_parser_write.hpp ${INSTALL_PATH_PREFIX}/include/boost/property_tree/detail/
+
 rm -rf ${BOOST_PATH}
