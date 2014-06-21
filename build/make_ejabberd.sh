c691f69b4cf9c229b14a925dd71b68966a556c47
wuyingfengsui <wuyingfengsui55@sina.com>
2014-06-21 17:13:26 +0800

    添加构建服务器脚本，完善编译ejabberd的脚本

--- build/make_ejabberd.sh
+++ build/make_ejabberd.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
+# 编译ejabberd，目前只支持debian类的Linux系统
+
+function red_echo()
+{
+	echo -e "\033[0;31;1m${1}\033[0m"
+}
 
 # check the caller is root
 if [ $HOME != "/root" ]; then
-	echo "ERROR: please run the script by root!"
+	red_echo "ERROR: please run the script by root!"
 	exit 100
 fi
 
 # check the necessary command
-NECESSARY_COMMANDS=(gcc make git apt-get)
+NECESSARY_COMMANDS=(apt-get gcc make git)
 for command in ${NECESSARY_COMMANDS[@]};
 do
-	type ${command} >/dev/null 2>&1 || { echo "ERROR: the ${command} can't be found."; exit 200; }
+	type ${command} >/dev/null 2>&1 || { red_echo "ERROR: the ${command} can't be found."; exit 200; }
 done
 
 # set up the source codes pathname
-SOURCE_PREFIX=$HOME
+SOURCE_PREFIX=`pwd`
 EXPAT_PATH=${SOURCE_PREFIX}/expat-2.1.0
 YAML_PATH=${SOURCE_PREFIX}/yaml-0.1.5
-ZLIB_PATH=${SOURCE_PREFIX}/zlib-1.2.8
-EJABBERD_PATH=${SOURCE_PREFIX}/ejabberd-13.12
+EJABBERD_PATH=${SOURCE_PREFIX}/ejabberd-14.05
 
 # check all the source package, then clear all the original source codes path
-SOURCE_PATHS=("${EXPAT_PATH}" "${YAML_PATH}" "${ZLIB_PATH}" "${EJABBERD_PATH}")
-PACKAGE_PATHS=("${EXPAT_PATH}.tar.gz" "${YAML_PATH}.tar.gz" "${ZLIB_PATH}.tar.gz" "${EJABBERD_PATH}.tgz")
+SOURCE_PATHS=("${EXPAT_PATH}" "${YAML_PATH}" "${EJABBERD_PATH}")
+PACKAGE_PATHS=("${EXPAT_PATH}.tar.gz" "${YAML_PATH}.tar.gz" "${EJABBERD_PATH}.tgz")
 
 for path in ${PACKAGE_PATHS[@]};
 do
 	if [ ! -f "${path}" ]; then
-		echo "ERROR: ${path} does't exist!"
+		red_echo "ERROR: ${path} does't exist!"
 		exit 1
 	fi
 done
@@ -35,35 +40,35 @@ done
 for path in ${SOURCE_PATHS[@]};
 do
 	if [ -d "${path}" ]; then
-		echo "delete the directory: ${path}"
+		red_echo "delete the directory: ${path}"
 		rm -rf ${path}
 	fi
 done
 
 # git clone some libraries
-if [ -d deps ]; then
-	echo "delete the directory: deps"
-	rm -rf deps
-fi
-mkdir deps
-cd deps
-# 经测试使用https速度更快些
-git clone https://github.com/processone/cache_tab.git p1_cache_tab || exit 2
-git clone https://github.com/processone/tls.git p1_tls || exit 2
-git clone https://github.com/processone/stringprep.git p1_stringprep || exit 2
-git clone https://github.com/processone/xml.git p1_xml || exit 2
-git clone https://github.com/processone/p1_yaml.git || exit 2
-git clone https://github.com/rds13/xmlrpc.git || exit 2
-git clone https://github.com/processone/zlib.git || exit 2
-git clone https://github.com/basho/lager.git || exit 2
-git clone https://github.com/processone/eiconv.git || exit 2
+# 提前下载一些依赖库，经测试国内使用https速度更快些
+# if [ -d deps ]; then
+# 	echo "delete the directory: deps"
+# 	rm -rf deps
+# fi
+# mkdir deps
+# cd deps
+# git clone https://github.com/processone/cache_tab.git p1_cache_tab || exit 2
+# git clone https://github.com/processone/tls.git p1_tls || exit 2
+# git clone https://github.com/processone/stringprep.git p1_stringprep || exit 2
+# git clone https://github.com/processone/xml.git p1_xml || exit 2
+# git clone https://github.com/processone/p1_yaml.git || exit 2
+# git clone https://github.com/rds13/xmlrpc.git || exit 2
+# git clone https://github.com/processone/zlib.git || exit 2
+# git clone https://github.com/basho/lager.git || exit 2
+# git clone https://github.com/processone/eiconv.git || exit 2
 
-git clone https://github.com/DeadZen/goldrush.git || exit 2
-cd goldrush
-git checkout 0.1.6 || exit 2
-cd ..
+# git clone https://github.com/DeadZen/goldrush.git || exit 2
+# cd goldrush
+# git checkout 0.1.6 || exit 2
+# cd ..
 
-cd ..
+# cd ..
 
 # 添加erlang最新源
 grep -q erlang-solutions /etc/apt/sources.list
@@ -83,73 +88,68 @@ fi
 
 apt-get install -y --force-yes erlang
 if [ $? -ne 0 ]; then
-	echo "apt-get erlang failed."
+	red_echo "apt-get erlang failed."
 	exit 2
 fi
 
 apt-get install -y --force-yes libcurl4-openssl-dev
 if [ $? -ne 0 ]; then
-	echo "apt-get openssl failed."
+	red_echo "apt-get openssl failed."
 	exit 2
 fi
 
 
 # start compiling
-echo "compiling expat..."
+red_echo "compiling expat..."
 tar zxf ${EXPAT_PATH}.tar.gz
 cd ${EXPAT_PATH}
 ./configure
 make;make install
 if [ $? -ne 0 ]; then
-	echo "failed."
+	red_echo "compile expat failed."
 	exit 2
 fi
 
 cd -
 rm -rf ${EXPAT_PATH}
 
-echo "compiling YAML..."
+red_echo "compiling YAML..."
 tar zxf ${YAML_PATH}.tar.gz
 cd ${YAML_PATH}
 ./configure
 make;make install
 if [ $? -ne 0 ]; then
-	echo "failed."
+	red_echo "compile YAML failed."
 	exit 2
 fi
 
 cd -
 rm -rf ${YAML_PATH}
 
-echo "compiling zlib..."
-tar zxf ${ZLIB_PATH}.tar.gz
-cd ${ZLIB_PATH}
-./configure
-make;make install
-if [ $? -ne 0 ]; then
-	echo "failed."
-	exit 2
-fi
-
-cd -
-rm -rf ${ZLIB_PATH}
-
-echo "compiling ejabberd..."
+red_echo "compiling ejabberd..."
 tar zxf ${EJABBERD_PATH}.tgz
 cd ${EJABBERD_PATH}
 ./configure
 if [ $? -ne 0 ]; then
-	echo "configure failed."
+	red_echo "configure ejabberd failed."
 	exit 2
 fi
 
-mv ../deps .
+# 将提前下好的库移到指定目录
+if [ -d ../deps ]; then
+	mv ../deps .
+fi
 
 make;make install
 if [ $? -ne 0 ]; then
-	echo "make failed."
+	red_echo "make ejabberd failed."
 	exit 2
 fi
 
 cd -
 rm -rf ${EJABBERD_PATH}
+
+# 避免运行时可能出现找不到依赖库的情况
+ldconfig
+
+red_echo "Finish!"
---
+++ build/make_server.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+# 此脚本用来构建Psychokinesis服务器运行所需要的环境
+# 基于Linux，请在安装有包管理器（apt/yum）的Linux系统上运行
+# 软件列表：
+# Web服务器（静态资源）：nginx
+# Web服务器（动态资源）：nodejs
+# 数据库：MongoDB
+# 即时通讯服务器：ejabberd（由于添加插件需要重新编译，使用make_ejabberd.sh安装）
+
+SOFTWARE=(mongodb-org npm nodejs nginx)
+
+function red_echo()
+{
+	echo -e "\033[0;31;1m${1}\033[0m"
+}
+
+function read_string()
+{
+    while :
+    do
+        read -p "$1"
+        if [ "${REPLY}" != "" ]; then
+            break
+        fi
+    done
+
+    echo ${REPLY}
+}
+
+function path_available()
+{
+	sudo find $1 -type f -exec chmod 644 {} \;
+	sudo find $1 -type d -exec chmod 755 {} \;
+}
+
+
+# 查找系统可用的包管理器
+type aptitude >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	INSTALLER=aptitude
+fi
+
+type yum >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	INSTALLER=yum
+fi
+
+if [ "${INSTALLER}" = "" ]; then
+	red_echo "No package installer found!"
+	exit 1
+fi
+
+# 导入需要用到的一些库
+if [ "${INSTALLER}" = "yum" ]; then
+	# MongoDB
+	if [ ! -f /etc/yum.repos.d/mongodb.repo ];then
+		echo "[MongoDB]
+		name=MongoDB Repository
+		baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64
+		gpgcheck=0
+		enabled=1" | sudo tee -a /etc/yum.repos.d/mongodb.repo
+	fi
+elif [ "${INSTALLER}" = "aptitude" ]; then
+	# MongoDB
+	if [ ! -f /etc/apt/sources.list.d/mongodb.list ];then
+		sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+		echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+	fi
+fi
+
+sudo ${INSTALLER} update -y
+
+# 依次安装软件
+for soft in ${SOFTWARE[@]};
+do
+	if [ "${INSTALLER}" = "aptitude" ]; then
+		cat /etc/issue | grep -q "Debian"
+		if [ $? -eq 0 ]; then
+			# nodejs和npm在Debian库中还没有，需要从官网直接下载
+			if [ "${soft}" = "npm" ] || [ "${soft}" = "nodejs" ]; then
+				INSTALL_NODEJS_FROM_SOURCE="yes"
+				continue
+			fi
+		fi
+		
+		sudo ${INSTALLER} install -y ${soft}
+		RET=$?
+	elif [ "${INSTALLER}" = "yum" ]; then
+		sudo ${INSTALLER} install -y ${soft}
+		RET=$?
+	fi
+	
+	if [ ${RET} -ne 0 ]; then
+		red_echo "Install ${soft} failed."
+		exit 2
+	fi
+done
+
+if [ "${INSTALL_NODEJS_FROM_SOURCE}" = "yes" ]; then
+	# 从官网直接下载安装nodejs
+	src=$(mktemp -d) && cd $src
+	uname -a | grep -q x86_64
+	if [ $? -eq 0 ]; then
+		NODEJS_URL=http://nodejs.org/dist/v0.10.29/node-v0.10.29-linux-x64.tar.gz
+	else
+		NODEJS_URL=http://nodejs.org/dist/v0.10.29/node-v0.10.29-linux-x86.tar.gz
+	fi
+	
+	wget -N ${NODEJS_URL}
+	if [ $? -ne 0 ]; then
+		red_echo "Download nodejs failed."
+		exit 2
+	fi
+	
+	tar xzf node-v* && cd node-v*
+	if [ $? -ne 0 ]; then
+		red_echo "Uncompress nodejs failed."
+		exit 2
+	fi
+	
+	sudo cp -ru bin /usr
+	sudo cp -ru lib /usr
+	sudo cp -ru share /usr
+	sudo cp -ru include /usr
+	
+	cd -
+	
+	# 安装npm
+	sudo aptitude install -y curl
+	if [ $? -ne 0 ]; then
+		red_echo "Install curl failed. Npm's installation need it."
+		exit 2
+	fi
+	
+	curl https://www.npmjs.org/install.sh | sudo sh
+	if [ $? -ne 0 ]; then
+		red_echo "Install npm failed."
+		exit 2
+	fi
+fi
+
+red_echo "All the software that psychokinesis required has been installed."
